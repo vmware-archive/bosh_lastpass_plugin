089378f40fec50ea0d2ec6eec0e8908cb364a1c9
Jatin Naik <jsnaik@gmail.com>
2016-05-18 23:29:33 +0100

    Handled command not found

--- lib/bosh/lastpass_plugin/additions.rb
+++ lib/bosh/lastpass_plugin/additions.rb
@@ -10,6 +10,11 @@ module Bosh
               exit($?.exitstatus)
             end
             output
+          rescue Errno::ENOENT => e
+            say "Error: BOSH Lastpass Error lastpass cli not found"
+            say "Error: Get cli from https://github.com/lastpass/lastpass-cli and ensure its in the PATH"
+            say "Error: #{e}"
+            exit($?.exitstatus)
           end
         end
       end
